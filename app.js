ea02cbb999ca76ddecde7333fce38364e82b9bc4
Talha Mirza <talharmirza@gmail.com>
2025-11-12 23:47:27 -0500

    Error Handling for js file

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const fetchData = async () => {
 
     try {
 
-        const response = await fetch("https://api.sampleapis.com/coffee/hot")
-        const data = await response.json()
-        console.log(data)
+        const response = await fetch("https://api.sampleapis.com/coffee/hot");
+        
+        if (!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+        
+        const data = await response.json();
+        console.log(data);
 
     } catch (err) {
 
-        console.error(err)
+        console.error(err);
 
     }
 
 }
 
-fetchData()
+fetchData();
